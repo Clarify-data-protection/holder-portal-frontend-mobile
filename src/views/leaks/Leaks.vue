a12c32cd525c0c30600c7380f237c3c6955557e9
Samuel Ferracini <samferracini@gmail.com>
2022-09-10 12:42:48 -0300

    feat: create leak card

--- src/views/leaks/Leaks.vue
+++ src/views/leaks/Leaks.vue
@@ -1,14 +1,37 @@
 <template>
-  <v-col cols="12" class="pa-0" align="center">
-    <v-card color="#385F73">
-      <v-card-text>
-        <span style="color: white">
-          Facebook
-        </span>
-      </v-card-text>
-    </v-card>
-  </v-col>
+  <div class="wrap">
+    <v-text-field
+      outlined
+      dense
+      placeholder="Pesquise..."
+      label="Pesquise..."
+      hide-details="auto"
+    />
+
+    <leak-card v-for="(leak, index) in leaks" :key="index"></leak-card>
+  </div>
 </template>
 
-<script></script>
-<style lang="scss" scoped></style>
+<script>
+import LeakCard from './components/LeakCard.vue'
+
+export default {
+  components: {
+    LeakCard
+  },
+
+  data() {
+    return {
+      leaks: Array.from({ length: 50 }, (_, i) => i)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
