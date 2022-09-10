b41888bbf2f3a10fde469061e702c865c8bebb63
Samuel Ferracini <samferracini@gmail.com>
2022-09-10 12:42:51 -0300

    refactor: integrate nav bar with app bar

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,25 +1,48 @@
 <template>
   <v-app>
-    <v-app-bar app flat absolute color="primary">
-      <div class="boxed-container w-full">
-        <div class="d-flex align-center mx-6">
+    <v-app-bar extended fixed app color="primary">
+      <v-col>
+        <v-row>
           <v-spacer></v-spacer>
+          <div class="d-flex align-center mx-6">
+            <v-spacer></v-spacer>
 
-          <theme-switcher></theme-switcher>
-          <v-btn icon small class="ms-3">
-            <v-badge :content="2" color="error" overlap>
-              <v-icon color="white">
-                mdi-bell-outline
+            <theme-switcher></theme-switcher>
+            <v-btn icon small class="ms-3">
+              <v-badge :content="2" color="error" overlap>
+                <v-icon color="white">
+                  mdi-bell-outline
+                </v-icon>
+              </v-badge>
+            </v-btn>
+            <app-bar-user-menu></app-bar-user-menu>
+          </div>
+        </v-row>
+        <v-row>
+          <div class="navbar">
+            <v-btn
+              v-if="goBack"
+              elevation="0"
+              x-small
+              absolute
+              left
+              @click="$router.go(-1)"
+              color="transparent"
+              class="white--text"
+            >
+              <v-icon size="30">
+                mdi-chevron-left
               </v-icon>
-            </v-badge>
-          </v-btn>
-          <app-bar-user-menu></app-bar-user-menu>
-        </div>
-      </div>
+            </v-btn>
+            <span>
+              {{ currentPage }}
+            </span>
+          </div>
+        </v-row>
+      </v-col>
     </v-app-bar>
 
     <v-main>
-      <nav-bar />
       <div class="app-content-container boxed-container pa-6">
         <slot></slot>
       </div>
@@ -32,18 +55,22 @@
 import ThemeSwitcher from './components/ThemeSwitcher.vue'
 import AppBarUserMenu from './components/AppBarUserMenu.vue'
 import BottomTabNavigation from './components/BottomTabNavigation'
-import NavBar from './components/NavBar.vue'
 
 export default {
   components: {
     ThemeSwitcher,
     AppBarUserMenu,
-    BottomTabNavigation,
-    NavBar
+    BottomTabNavigation
   },
   computed: {
     isBottomVisible: function() {
       return !this.$route.meta.hideTabsNavigator
+    },
+    currentPage: function() {
+      return this.$route.meta.title || ''
+    },
+    goBack: function() {
+      return this.$route.meta.goBack
     }
   }
 }
@@ -52,19 +79,21 @@ export default {
 <style lang="scss" scoped>
 .v-app-bar ::v-deep {
   .v-toolbar__content {
-    padding: 0;
-
-    .app-bar-search {
-      .v-input__slot {
-        padding-left: 18px;
-      }
-    }
+    padding: 10px 0px;
+    height: auto !important;
   }
 }
 
-.boxed-container {
-  max-width: 1440px;
-  margin-left: auto;
-  margin-right: auto;
+.navbar {
+  width: 100%;
+  padding-bottom: 5px;
+  background: var(--v-primary-base);
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/views/leaks/components/LeakCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card ripple color="error">
+    <v-card-text class="white--text">
+      <v-col>
+        <v-row align="center">
+          <span class="font-weight-bold">
+            Facebook
+          </span>
+          <v-spacer></v-spacer>
+          <span>12/12/1212</span>
+        </v-row>
+      </v-col>
+    </v-card-text>
+  </v-card>
+</template>
